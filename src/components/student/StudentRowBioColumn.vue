5737d0de197c2ee101d2fcc6e667128cfc986dff
Pauline Kerschen <kerschen@berkeley.edu>
2024-11-13 15:45:12 -0800

    Revert "[QA] BOAC-4859, link to degree check from cohort/group listing page"
    This reverts commit 3520961fcf3b23177ed27d40e9acdddc566b42d1.

--- src/components/student/StudentRowBioColumn.vue
+++ src/components/student/StudentRowBioColumn.vue
@@ -143,11 +143,6 @@
         <span v-if="student.athleticsProfile.isActiveAsc === false"> (Inactive)</span>
       </div>
     </div>
-    <div v-if="currentUser.canReadDegreeProgress" class="font-size-13 font-weight-medium">
-      <router-link :id="`degree-checks-of-${student.sid}`" :to="getDegreeCheckPath(student)">
-        Degree Checks<span class="sr-only"> of {{ student.name }} (will open new browser tab)</span>
-      </router-link>
-    </div>
   </div>
 </template>
 
