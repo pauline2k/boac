508da060812e6251d801fe8c490fee998ec889e8
John Crossman <johncrossman@berkeley.edu>
2025-01-07 10:28:24 -0800

    BOAC-5788, validation and UX tidying in /cohort 'Rename'

--- src/components/cohort/RenameCohort.vue
+++ src/components/cohort/RenameCohort.vue
@@ -1,33 +1,46 @@
 <template>
-  <v-card flat>
-    <div class="align-center d-flex flex-wrap">
+  <v-card class="w-100" flat>
+    <div class="align-start d-flex flex-wrap">
       <v-text-field
         id="rename-cohort-input"
         v-model="name"
+        :aria-invalid="!name"
         aria-label="Cohort name"
-        class="v-input-details-override mr-3"
-        counter="255"
+        class="v-input-details-override mb-1 mr-3"
         density="comfortable"
         :disabled="isSaving"
-        hide-details
         label="Cohort Name"
         :maxlength="maxlength"
         persistent-counter
         required
-        :rules="[validationRules.valid]"
+        :rules="[() => isValidName]"
         validate-on="lazy input"
         @keyup.enter="submit"
         @keyup.esc="cancel"
-      />
+      >
+        <template #details>
+          <div class="pt-1">
+            {{ size(name) ? `${maxlength} character limit (${maxlength - size(name)} left)` : `${maxlength} character limit` }}
+          </div>
+        </template>
+      </v-text-field>
+      <span
+        v-if="size(name) === maxlength"
+        aria-live="polite"
+        class="sr-only"
+        role="alert"
+      >
+        Cohort name cannot exceed {{ maxlength }} characters.
+      </span>
       <div class="d-flex justify-end">
         <ProgressButton
           id="rename-cohort-confirm"
           :action="submit"
           aria-label="Rename Cohort"
-          :disabled="isInvalid || isSaving"
+          :disabled="isValidName !== true || isSaving"
           :in-progress="isSaving"
           size="large"
-          text="Rename"
+          :text="isSaving ? 'Renaming' : 'Rename'"
         />
         <v-btn
           id="rename-cohort-cancel"
@@ -41,17 +54,6 @@
         />
       </div>
     </div>
-    <div id="name-cohort-counter" class="text-left font-size-13 text-no-wrap ml-4 mt-1">
-      <span class="sr-only">Cohort name has a </span>{{ maxlength }} character limit <span v-if="size(name)">({{ maxlength - size(name) }} left)</span>
-      <span
-        v-if="size(name) === 255"
-        aria-live="polite"
-        class="sr-only"
-        role="alert"
-      >
-        Cohort name cannot exceed 255 characters.
-      </span>
-    </div>
   </v-card>
 </template>
 
@@ -59,7 +61,7 @@
 import ProgressButton from '@/components/util/ProgressButton'
 import {alertScreenReader} from '@/lib/utils'
 import {putFocusNextTick, setPageTitle} from '@/lib/utils'
-import {onMounted, ref} from 'vue'
+import {computed, onMounted, ref} from 'vue'
 import {saveCohort} from '@/api/cohort'
 import {size} from 'lodash'
 import {storeToRefs} from 'pinia'
@@ -68,17 +70,10 @@ import {validateCohortName} from '@/lib/cohort'
 
 const cohortStore = useCohortStore()
 const {cohortId, cohortName, filters} = storeToRefs(cohortStore)
-const isInvalid = ref(true)
 const isSaving = ref(false)
+const isValidName = computed(() => validateCohortName({id: cohortId.value, name: name.value}))
 const maxlength = ref(255)
 const name = ref(undefined)
-const validationRules = {
-  valid: name => {
-    const valid = validateCohortName({id: cohortId.value, name})
-    isInvalid.value = true !== valid
-    return valid
-  }
-}
 
 onMounted(() => {
   name.value = cohortName.value
